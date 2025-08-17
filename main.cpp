162c9cdc69f403aa0354d2674561995f8d0570b2
Hariprasath Krishnamoorthi <hariprasath.krishnamoorthi@psiog.com>
2025-08-17 14:00:31 -0700

    Second  commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
 using namespace std;
 
+bool securityScreening() {
+    char ans;
+    cout << "Restricted articles? (y/n): ";
+    cin >> ans;
+    if (ans == 'y' || ans == 'Y') cout << "Give up restricted articles\n";
+
+    cout << "Metal detected? (y/n): ";
+    cin >> ans;
+    if (ans == 'y' || ans == 'Y') cout << "Hand search\n";
+
+    cout << "Dangerous goods? (y/n): ";
+    cin >> ans;
+    if (ans == 'y' || ans == 'Y') cout << "Give up dangerous goods\n";
+
+    cout << "Screening cleared\n";
+    return true;
+}
+
+
 
 int main() {
     cout << "Check-in\n";
